0fbda973d6c9aad8988fd44af17af9e99d63fd9a
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2024-10-03 19:08:56 +0530

    updated UI

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import './style.css';
+import pizzaImage from './pizza_img.webp';
 
 export default function makeHomePage() {
     const homePage = document.createElement("div");
@@ -14,9 +15,8 @@ export default function makeHomePage() {
     const IntroPara = document.createElement('p');
     IntroPara.textContent = "Welcome to our cozy pizza restaurant, where every slice tells a delicious story! Whether you're craving a classic Margherita or adventurous with our specialty toppings, we promise a slice of perfection in every bite. Join us for a culinary journey where our handcrafted pizzas, made from the finest ingredients and baked to perfection, await to delight your taste buds. From our kitchen to your table, experience the essence of authentic flavors and warm hospitality at our pizza paradise.";
 
-
     const imgElement = document.createElement('img');
-    imgElement.src = './pizza_img.webp';
+    imgElement.src = pizzaImage;
 
 
     homePage.appendChild(title);
